437439362f31148dc2ec6d8193dedf7eae6b3af2
Cloudlead20 <152505623+Cloudlead20@users.noreply.github.com>
2024-02-04 09:52:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('docker, Node.js!\n');
+    res.end('docker1, Node.js!\n');
 });
 
 const PORT = 5000;
